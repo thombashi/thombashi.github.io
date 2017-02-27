31bebec9dc5f3bf815b47c45c7e7b34d4f1def98
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-02-27 22:27:33 +0900

    Update

--- js/write_items.js
+++ js/write_items.js
@@ -2,7 +2,7 @@ function writeCardItems(cardNameArray) {
     cardNameArray.forEach(function (cardName, i, a) {
         document.write("<div class='item'>");
         document.write(
-            "<div class='ghcard' src='data/" + cardName + ".json' emoji='resolve'></div>");
+            "<div class='ghscard' src='data/" + cardName + ".json'></div>");
         document.write('</div>');
     });
 }

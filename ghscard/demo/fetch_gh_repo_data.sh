38902982e351b335c14e1d2f66b635c000ff74c5
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-04-01 23:55:29 +0900

    Initial commit

---
+++ ghscard/demo/fetch_gh_repo_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+output_dir="data"
+base_command="ghscard gen"
+targets=(
+    "freeCodeCamp/freeCodeCamp"
+    "twbs/bootstrap"
+    "vhf/free-programming-books"
+    "facebook/react"
+    "d3/d3"
+    "angular/angular.js"
+    "sindresorhus/awesome"
+    "getify/You-Dont-Know-JS"
+    "tensorflow/tensorflow"
+    "robbyrussell/oh-my-zsh"
+    "airbnb/javascript"
+    "FortAwesome/Font-Awesome"
+    "github/gitignore"
+    "vuejs/vue"
+    "facebook/react-native"
+    "jquery/jquery"
+    "electron/electron"
+    "torvalds/linux"
+    "docker/docker"
+    "daneden/animate.css"    
+)
+
+command="${base_command} -o ${output_dir} ${targets[@]}"
+echo "${command}"
+${command}

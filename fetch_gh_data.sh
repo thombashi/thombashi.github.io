ef7031123017aeb81fb93ee2721876792ee5a9e8
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-04-02 22:33:13 +0900

    Initial commit

---
+++ fetch_gh_data.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+output_dir="data"
+base_command="ghscard gen"
+targets=(
+    "thombashi"
+
+    "thombashi/sqlitebiter"
+    "thombashi/tcconfig"
+
+    "thombashi/ghscard"
+
+    "thombashi/appconfigpy"
+    "thombashi/DataProperty"
+    "thombashi/DateTimeRange"
+    "thombashi/mbstrdecoder"
+    "thombashi/pathvalidate"
+    "thombashi/pingparsing"
+    "thombashi/pytablereader"
+    "thombashi/pytablewriter"
+    "thombashi/SimpleSQLite"
+    "thombashi/sqliteschema"
+    "thombashi/subprocrunner"
+    "thombashi/typepy"
+
+    "thombashi/allpairspy"
+    "thombashi/NFStest"
+    
+    "thombashi/PythonExamples"
+)
+
+command="${base_command} -o ${output_dir} ${targets[@]}"
+echo "${command}"
+${command}
+

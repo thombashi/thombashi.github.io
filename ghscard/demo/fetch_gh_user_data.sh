131eac4d42cc30b3c289fc05f8b73dbb5e0012e6
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-04-02 09:41:36 +0900

    Initial commit

---
+++ ghscard/demo/fetch_gh_user_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+output_dir="data"
+base_command="ghscard gen"
+targets=(
+    "freeCodeCamp"
+    "twbs"
+    "vhf"
+    "facebook"
+    "d3"
+    "angular"
+    "sindresorhus"
+    "getify"
+    "tensorflow"
+    "robbyrussell"
+    "airbnb"
+    "FortAwesome"
+    "github"
+    "vuejs"
+    "facebook"
+    "jquery"
+    "electron"
+    "torvalds"
+    "docker"
+    "daneden"
+)
+
+command="${base_command} -o ${output_dir} ${targets[@]}"
+echo "${command}"
+${command}
